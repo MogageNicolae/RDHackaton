989f532da1f38e5d3dd59d33541ac1593a47ff2b
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-10-18 12:31:58 +0300

    Add front end

--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -3,7 +3,7 @@ const baseURL = 'http://localhost:5000';
 export const registerUser = async (userData) => {
     try {
         console.log(JSON.stringify(userData));
-        const response = await fetch(`${baseURL}/register`, {
+        const response = await fetch(`${baseURL}/auth/register`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(userData)
@@ -25,7 +25,7 @@ export const registerUser = async (userData) => {
 export const loginUser = async (credentials) => {
     try {
         console.log("service loging, see credentials: ", credentials);
-        const response = await fetch(`${baseURL}/login`, {
+        const response = await fetch(`${baseURL}/auth/login`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(credentials)
@@ -40,12 +40,12 @@ export const loginUser = async (credentials) => {
     }
 };
 
-export const createChatRoom = async (users) => {
+export const createChatRoom = async (users, token) => {
     try {
         console.log("creating chatroom with users: ", users);
         const response = await fetch(`${baseURL}/chat/create`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
             body: JSON.stringify(users)
         });
         if (!response.ok) {
@@ -58,12 +58,12 @@ export const createChatRoom = async (users) => {
     }
 } 
 
-export const sendMessageInChat = async (messageData) => {
+export const sendMessageInChat = async (messageData, token) => {
     try {
         console.log("sending message in chatroom: ", messageData);
-        const response = await fetch(`${baseURL}/chat/send_message`, {
+        const response = await fetch(`${baseURL}/chat/send_message/text`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
             body: JSON.stringify(messageData)
         });
         if (!response.ok) {
@@ -76,10 +76,13 @@ export const sendMessageInChat = async (messageData) => {
     }
 } 
 
-export const chatMessages = async (chatId) => {
+export const chatMessages = async (chatId, token, sender) => {
     try {
         console.log("getting chat messages from chat: ", chatId);
-        const response = await fetch(`${baseURL}/chat/${chatId}`);
+        const response = await fetch(`${baseURL}/chat/${chatId}?sender=${sender}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+        });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -90,10 +93,13 @@ export const chatMessages = async (chatId) => {
     }
 } 
 
-export const getAllChatRooms = async (name) => {
+export const getAllChatRooms = async (name, token) => {
     try {
         console.log("getting chat rooms for user with name: ", name);
-        const response = await fetch(`${baseURL}/chat/history?username=${encodeURIComponent(name)}`);
+        const response = await fetch(`${baseURL}/chat/history?username=${encodeURIComponent(name)}`,{
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+        } );
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -103,4 +109,19 @@ export const getAllChatRooms = async (name) => {
         throw error;
     }
 }
+
+export const searchForUsers = async (name) => {
+    try {
+        console.log("searching for users with name: ", name);
+        const response = await fetch(`${baseURL}/user/${encodeURIComponent(name)}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Searching for users failed: ", error);
+        throw error;
+    }
+}
+
 
